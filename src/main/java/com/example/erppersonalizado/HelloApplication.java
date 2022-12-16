fba9366b7a4e5d61bca791436a0a53e76615cf9c
valgaba <valgababueno@safareyes.es>
2022-12-16 13:46:13 +0100

    GGgggg

--- src/main/java/com/example/erppersonalizado/HelloApplication.java
+++ src/main/java/com/example/erppersonalizado/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!"); // Que pasa juanffffff
+        //holaaa
         stage.setScene(scene);
         stage.show();
     }
