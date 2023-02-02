d0ca9f78c9525b4213ee95b9b9531d08a5373112
Ivan <ivangarciajm@gmail.com>
2023-02-02 12:43:40 +0100

    estructura interfaz casi hecha, aun sin css

--- src/main/java/com/example/erppersonalizado/HelloApplication.java
+++ src/main/java/com/example/erppersonalizado/HelloApplication.java
@@ -12,8 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!"); // Que pasa juanffffff
-        //holaaa
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
