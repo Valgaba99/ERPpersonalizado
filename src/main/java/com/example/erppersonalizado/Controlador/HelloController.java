48b26f1fcd114d3e96a973bf87998ed183c81dd8
Ivan <ivangarciajm@gmail.com>
2023-02-03 12:18:50 +0100

    interfaz casi terminada

---
+++ src/main/java/com/example/erppersonalizado/Controlador/HelloController.java
@@ -0,0 +1,37 @@
+package com.example.erppersonalizado.Controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+    public class HelloController {
+
+        @FXML
+        private Button bLogin;
+
+        @FXML
+        private Button bRegistro;
+
+        @FXML
+        private Pane pInicio;
+
+        @FXML
+        private Pane pMedico;
+
+        @FXML
+        private Pane pMedico1;
+
+        @FXML
+        private Pane pMedico2;
+
+        @FXML
+        private Pane pMedico21;
+
+        @FXML
+        private TextField tfContraseña;
+
+        @FXML
+        private TextField tfEmail;
+    }
+
